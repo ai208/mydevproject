5e45145b83069571d7a838a4c3d8e0b327933404
ai208 <yasu6328hand@gmail.com>
2026-01-05 22:46:40 +0900

    8-1-2 fin create

--- weightapp_cbv/urls.py
+++ weightapp_cbv/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('list/',views.HealthcareListView.as_view(),name='healthcare_list'),
     path('<int:pk>/',views.HealthcareDetailView.as_view(),name='healthcare_detail'),
+    path('new/',views.HealthcareCreateView.as_view(),name = 'healthcare_create'),
 ]
--- weightapp_cbv/views.py
+++ weightapp_cbv/views.py
@@ -1,5 +1,6 @@
 from . import models
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView,CreateView
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -12,3 +13,10 @@ class HealthcareDetailView(DetailView):
     model = models.Healthcare
     template_name = 'weightapp_cbv/healthcare_detail.html'
     context_object_name='healthcare'
+
+class HealthcareCreateView(CreateView):
+    model = models.Healthcare
+    template_name = 'weightapp_cbv/healthcare_create.html'
+    fields = [    'physical_health','mental_health','eat_three_meals','exercise_time','sleep_time','weight','memo']
+    success_url = reverse_lazy('healthcare_list')
+
