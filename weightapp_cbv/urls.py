37ee1988d99bcca090ce5f761ab07c9045724092
ai208 <yasu6328hand@gmail.com>
2026-01-05 22:20:16 +0900

    ch7 fin

--- weightapp_cbv/urls.py
+++ weightapp_cbv/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('list/',views.HealthcareListView.as_view(),name='healthcare_list'),
+    path('<int:pk>/',views.HealthcareDetailView.as_view(),name='healthcare_detail'),
 ]
--- weightapp_cbv/views.py
+++ weightapp_cbv/views.py
@@ -1,5 +1,5 @@
 from . import models
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 
 # Create your views here.
@@ -7,3 +7,8 @@ class HealthcareListView(ListView):
     model = models.Healthcare
     template_name = 'weightapp_cbv/healthcare_list.html'
     context_object_name='healthcares'
+
+class HealthcareDetailView(DetailView):
+    model = models.Healthcare
+    template_name = 'weightapp_cbv/healthcare_detail.html'
+    context_object_name='healthcare'
